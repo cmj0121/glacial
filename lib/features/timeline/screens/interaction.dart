ef046ecd412eedf9a9a51b6b97a8bbaaec9ed12d
cmj <cmj@cmj.tw>
2025-05-19 13:36:57 +0800

    feat(timeline|interaction): show the interaction' state

--- lib/features/timeline/screens/interaction.dart
+++ lib/features/timeline/screens/interaction.dart
@@ -27,11 +27,13 @@ class InteractionBar extends StatelessWidget {
 class Interaction extends ConsumerStatefulWidget {
   final StatusSchema schema;
   final StatusInteraction action;
+  final double iconSize;
 
   const Interaction({
     super.key,
     required this.schema,
     required this.action,
+    this.iconSize = 24,
   });
 
   @override
@@ -43,12 +45,61 @@ class _InteractionState extends ConsumerState<Interaction> {
   Widget build(BuildContext context) {
     final String? accessToken = ref.read(currentAccessTokenProvider);
     final bool isEnabled = accessToken != null || widget.action.supportAnonymous;
+    final Color color = iconColor ?? Theme.of(context).colorScheme.secondary;
+    final TextStyle? style = Theme.of(context).textTheme.bodySmall?.copyWith(
+      color: color,
+      fontSize: 12,
+      fontWeight: FontWeight.bold,
+    );
 
-
-    return IconButton(
-      icon: Icon(widget.action.icon),
+    return TextButton.icon(
+      label: count != null ? Text("$count", style: style) : const SizedBox.shrink(),
+      icon: Icon(icon, color: color, size: widget.iconSize),
       onPressed: isEnabled ? () {} : null,
     );
+  }
+
+  // Get the icon baed on the action.
+  IconData get icon {
+    switch (widget.action) {
+      case StatusInteraction.reblog:
+        return (widget.schema.reblogged ?? false) ? widget.action.activeIcon : widget.action.icon;
+      case StatusInteraction.favourite:
+        return (widget.schema.favourited ?? false) ? widget.action.activeIcon : widget.action.icon;
+      case StatusInteraction.bookmark:
+        return (widget.schema.bookmarked ?? false) ? widget.action.activeIcon : widget.action.icon;
+      default:
+        return widget.action.icon;
+    }
+  }
+
+  // Get the color of the icon based on the action and the status
+  // interaction state.
+  Color? get iconColor {
+    switch (widget.action) {
+      case StatusInteraction.reblog:
+        return (widget.schema.reblogged ?? false) ? Theme.of(context).colorScheme.tertiary : null;
+      case StatusInteraction.favourite:
+        return (widget.schema.favourited ?? false) ? Theme.of(context).colorScheme.tertiary : null;
+      case StatusInteraction.bookmark:
+        return (widget.schema.bookmarked ?? false) ? Theme.of(context).colorScheme.tertiary : null;
+      default:
+        return null;
+    }
+  }
+
+  // Get the count of interaction based on the action.
+  int? get count {
+    switch (widget.action) {
+      case StatusInteraction.reply:
+        return widget.schema.repliesCount;
+      case StatusInteraction.reblog:
+        return widget.schema.reblogsCount;
+      case StatusInteraction.favourite:
+        return widget.schema.favouritesCount;
+      default:
+        return null;
+    }
   }
 }
 
