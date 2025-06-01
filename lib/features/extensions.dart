7d7f5ca3331c99e166a41cab34bc62aa717dd54e
cmj <cmj@cmj.tw>
2025-06-01 16:46:46 +0800

    refactor(core): move the extensions.dart into lib/features/

---
+++ lib/features/extensions.dart
@@ -0,0 +1,10 @@
+// The core exposed extensions for the Glacial app.
+
+export 'auth/extensions.dart';
+export 'account/extensions.dart';
+export 'explore/extensions.dart';
+export 'mastodon/extensions.dart';
+export 'timeline/extensions.dart';
+export 'trends/extensions.dart';
+
+// vim: set ts=2 sw=2 sts=2 et:
