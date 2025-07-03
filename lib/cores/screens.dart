fee1f7ab6a3abe2b95b915a0e4a2317a21f04307
cmj <cmj@cmj.tw>
2025-07-03 16:52:06 +0800

    style(core): the general NoResult widget

--- lib/cores/screens.dart
+++ lib/cores/screens.dart
@@ -728,4 +728,44 @@ class _SensitiveViewState extends State<SensitiveView> {
   }
 }
 
+// The widget to show the search result is empty, and show the
+// message to the user.
+class NoResult extends StatelessWidget {
+  final String message;
+  final double size;
+  final IconData icon;
+
+  const NoResult({
+    super.key,
+    this.message = "",
+    this.size = 64,
+    this.icon = Icons.sentiment_dissatisfied_outlined,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: buildContent(context),
+      ),
+    );
+  }
+
+  Widget buildContent(BuildContext context) {
+    final Color color = Theme.of(context).colorScheme.secondaryContainer;
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Icon(icon, size: size, color: color),
+        const SizedBox(height: 8),
+        Text(message, style: Theme.of(context).textTheme.bodyLarge),
+      ],
+    );
+  }
+}
+
 // vim: set ts=2 sw=2 sts=2 et:
